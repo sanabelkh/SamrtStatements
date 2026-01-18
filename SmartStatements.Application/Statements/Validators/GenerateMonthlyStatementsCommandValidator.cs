0daba6cf16c9d26d1ca97967623d8b300ad602e9
sanabelkh <97903874+sanabelkh@users.noreply.github.com>
2026-01-18 22:20:37 +0300

    Add files via upload

---
+++ SmartStatements.Application/Statements/Validators/GenerateMonthlyStatementsCommandValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using SmartStatements.Core.Statements.Commands;
+
+public class GenerateMonthlyStatementsCommandValidator
+    : AbstractValidator<GenerateMonthlyStatementsCommand>
+{
+    public GenerateMonthlyStatementsCommandValidator()
+    {
+        RuleFor(x => x.Month)
+            .NotEmpty().WithMessage(" Empty Failed !");
+            
+    }
+}
